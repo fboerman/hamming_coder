96c779adfd87e8b83b006b4e186d8b3b4d56ab1f
Williewonka <frankieby03@gmail.com>
2014-05-25 15:02:38 +0200

    made the encoder dynamic

--- hamming_code.py
+++ hamming_code.py
@@ -26,15 +26,19 @@ def BitString(symbol):
 if __name__ == '__main__': #python version of main()
     #define input arguments
     parser = argparse.ArgumentParser(description="encodes byte word of given symbol using hamming code")
-    parser.add_argument('--symbol', nargs='?', const=1, type=int, default=12, help='integer of ascii symbol (0-127)')
+    parser.add_argument('--symbol', nargs='?', const=1, type=int, default=77, help='integer of symbol that has to be encoded')
+    parser.add_argument('--length', nargs='?', const=1, type=int, default=8, help='minimum length of databits')
+
 
     #parse the input arguments
     SYMBOL = parser.parse_args().symbol
-    #clip the value of the symbol
-    if SYMBOL > 127:
-        SYMBOL = 127
-    elif SYMBOL < 0:
-        SYMBOL = 0
+    LENGTH = parser.parse_args().length
+
+    # #clip the value of the symbol
+    # if SYMBOL > 127:
+    #     SYMBOL = 127
+    # elif SYMBOL < 0:
+    #     SYMBOL = 0
 
     #we always do 4 parity bits because we have a byte word (8 bits can store all the symbols, actually 7 is already correct, but the assignment
         #specified 8)
@@ -44,15 +48,29 @@ if __name__ == '__main__': #python version of main()
     #the original bits
     bsymbol = BitString(SYMBOL)
     #append to 8 bits length
-    for i in range(0,8-FindBits(SYMBOL)):
+    for i in range(0,LENGTH-FindBits(SYMBOL)):
         bsymbol = '0' + bsymbol
-    #add the parity bits by slicing the string
-    bsymbol = 'xx' + bsymbol[:1] + 'x' + bsymbol[1:4] + 'x' + bsymbol[4:]
+
+    # bsymbol = 'xx' + bsymbol[:1] + 'x' + bsymbol[1:4] + 'x' + bsymbol[4:]
+
+    bsymbol = list(bsymbol)
+    #add the parity bits by slicing the string, by iterative trying to fit it in
+    i = 0
+    while 1:
+        j = pow(2,i)
+        if j > len(bsymbol):
+            break
+        bsymbol.insert(j-1,'x')
+        i += 1
+
+    bsymbol = "".join(bsymbol)
+    #count number of parities
+    PARITYNUM = bsymbol.count('x')
 
     #calculate the parity bits
     #dictionary to store parity strings
     parities = {}
-    for i in range(0,4):
+    for i in range(0,PARITYNUM):
         j = pow(2,i)
         parities[j] = ''
         #first create a string by using the use-skip method described here: https://www.youtube.com/watch?v=TYwrHiQ2-G4
@@ -64,7 +82,7 @@ if __name__ == '__main__': #python version of main()
             z += 1
 
     #now that we have the paritystrings, finally calculate the parity itself and put it in the result string
-    for i in range(0,4):
+    for i in range(0,PARITYNUM):
         j = pow(2,i)
         #because strings are immutable, temporary convert it to a list
         #than assign the parity bit at the parity position
